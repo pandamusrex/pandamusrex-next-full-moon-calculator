0711409b7f4968f8ac68b285f860341e3f4f2dba
pandamusrex <pandamusrex@gmail.com>
2025-09-01 08:15:27 -0700

    Initial commit

---
+++ pandamusrex-next-full-moon-calculator.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Plugin Name: PandamusRex Next Full Moon Calculator
+ * Version: 1.0.0
+ * Plugin URI: https://github.com/pandamusrex/pandamusrex-next-full-moon-calculator
+ * Description: PandamusRex Next Full Moon Widget
+ * Author: PandamusRex
+ * Author URI: https://www.github.com/pandamusrex/
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Requires at least: 6.4
+ * Requires PHP: 7.0
+ * Tested up to: 6.8
+ *
+ * Text Domain: pandamusrex-next-full-moon-calculator
+ * Domain Path: /lang/
+ *
+ * @package WordPress
+ * @author PandamusRex
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class PandamusRex_Next_Full_Moon_Calculator {
+    private static $instance;
+
+    public static function get_instance() {
+        if ( null == self::$instance ) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    private function __clone() {}
+
+    public function __wakeup() {}
+
+    public function __construct() {
+    }
+}
+
+PandamusRex_Next_Full_Moon_Calculator::get_instance();
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== PandamusRex Next Full Moon Calculator ===
+Contributors: pandamusrex
+Donate link: https://pandamusrex.com/
+Tags: woocommerce
+Requires at least: 6.4
+Tested up to: 6.8
+Stable tag: 1.0.0
+Requires PHP: 7.0
+License: GPLv2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Add a countdown to the next full moon to your blog! Awooo!
+
+== Description ==
+
+Add a countdown to the next full moon to your blog! Awooo!
+
+== Frequently Asked Questions ==
+
+= Is this safe for werewolves, wifwolves, and enbywolves to use? =
+
+Yes!
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
